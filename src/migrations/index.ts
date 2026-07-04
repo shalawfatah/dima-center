36313950bd387f3ca69e46400bd2dcdefd2c7e0d
Shalaw Fatah <shalaw.fatah@gmail.com>
2026-07-05 00:45:30 +0300

    migration removed

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260704_205950_add_missing_product_fields from './20260704_205950_add_missing_product_fields';
-
-export const migrations = [
-  {
-    up: migration_20260704_205950_add_missing_product_fields.up,
-    down: migration_20260704_205950_add_missing_product_fields.down,
-    name: '20260704_205950_add_missing_product_fields'
-  },
-];
