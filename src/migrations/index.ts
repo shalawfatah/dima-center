d8f95314607b63f15bc4107879aa324225978021
Shalaw Fatah <shalaw.fatah@gmail.com>
2026-07-30 17:21:14 +0300

    migration fail potential fix

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260730_134628_add_general_settings from './20260730_134628_add_general_settings';
+import * as migration_20260730_141915_add_general_settings_fields from './20260730_141915_add_general_settings_fields';
 
 export const migrations = [
   {
     up: migration_20260730_134628_add_general_settings.up,
     down: migration_20260730_134628_add_general_settings.down,
-    name: '20260730_134628_add_general_settings'
+    name: '20260730_134628_add_general_settings',
+  },
+  {
+    up: migration_20260730_141915_add_general_settings_fields.up,
+    down: migration_20260730_141915_add_general_settings_fields.down,
+    name: '20260730_141915_add_general_settings_fields'
   },
 ];
